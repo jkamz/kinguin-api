c4bda85b626f1d0e8f3f129692ebc5e306f3ee13
lukasz <lukasz.firek@cashbill.pl>
2018-10-27 14:53:34 +0200

    Adds simple axios instance creation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
-import Axios, {AxiosPromise} from 'axios'
+import Axios, { AxiosPromise, AxiosInstance } from 'axios'
 
-class API{
+const API_URL = {
+    production: "https://api2.kinguin.net",
+    sandbox: "https://api.api-sandbox.kinguin.info"
 }
 
-export default API
+class Kinguin {
+    axiosInstance: AxiosInstance;
+    constructor(isProd: boolean, version = "v1") {
+        this.axiosInstance = Axios.create({
+            baseURL: this.createUrl(isProd, version)
+        });
+    }
+
+    createUrl(isProd: boolean, version: string) {
+        return (isProd ? API_URL.production : API_URL.sandbox) + "/integration/" + version
+    }
+}
+
+export default Kinguin
 
 export {
-    API
+    Kinguin
 }
